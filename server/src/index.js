e4485eba47f7d336e5ee4c07e168cddeed3b377b
saramerault <sarah.merault@gmail.com>
2026-02-10 12:01:52 +0100

    initialisation DB

---
+++ server/src/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+
+ 
+
+const index = express();
+index.use(express.json())
+index.use(cors({
+    origin:'http://localhost:5173'
+}))
+
+const PORT = process.env.PORT || 5000;
+
+index.listen(PORT, () => {
+    console.log(`http://localhost:${PORT} `);
+});
+
